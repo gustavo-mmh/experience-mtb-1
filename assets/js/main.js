5d897548761d204c623c0e6e5ff376b192f01057
gustavo-mmh <gustavo-mmello@hotmil.com>
2022-08-18 00:48:31 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#dataNascimento").datepicker({
+        numberOfMonths: 1,
+        stepMonths: 1,
+        minDate: 0,
+        dayNamesMin: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    });
+});
