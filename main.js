2a8ccb4c4eb656fb4eefd2cfd8bf89aea70f9eb7
experience-mtb <113735174+experience-mtb@users.noreply.github.com>
2022-10-07 01:01:57 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { Cadastrar } from './cadastro/index.js';
-import { login } from './login/index.js';
-import { formLogin, loginDocumento, loginPais, loginPassword } from './ui.js';
-
-//--- Cadastro -----------------------------------------------------------------
-Cadastrar();
-login(formLogin, loginDocumento, loginPassword, loginPais);
-
