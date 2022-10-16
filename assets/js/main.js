986cdf47131c2b43eaa5807c04dea5a68f734be4
experience-mtb <113735174+experience-mtb@users.noreply.github.com>
2022-10-15 22:30:09 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,7 @@ import { formLogin, loginDocumento, loginPais, loginPassword } from './ui.js';
 
 //--- Cadastro -----------------------------------------------------------------
 Cadastrar();
-login(formLogin, loginDocumento, loginPassword, loginPais);
-
+formLogin.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    login(loginDocumento, loginPassword, loginPais);
+})
