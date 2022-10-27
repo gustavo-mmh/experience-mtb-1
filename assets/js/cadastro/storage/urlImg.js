83dd82a54a9304d295298bc4bcfd46806b3eef52
experience-mtb <113735174+experience-mtb@users.noreply.github.com>
2022-10-27 14:24:24 -0300

    Add files via upload

--- assets/js/cadastro/storage/urlImg.js
+++ assets/js/cadastro/storage/urlImg.js
@@ -1,31 +1,32 @@
 import { getDownloadURL, ref } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js";
 export function getUrlImage(storage, img, fotoCard) {
-    const starsRef = ref(storage, `images/${img}`);
+    let starsRef = ref(storage, `images/${img}`);
     // Get the download URL
-    getDownloadURL(starsRef)
-        .then((url) => {
-            // Insert url into an <img> tag to "download"
-            fotoCard.setAttribute('src', url);
-        })
-        .catch((error) => {
-            // A full list of error codes is available at
-            // https://firebase.google.com/docs/storage/web/handle-errors
-            switch (error.code) {
-                case 'storage/object-not-found':
-                    // File doesn't exist
-                    break;
-                case 'storage/unauthorized':
-                    // User doesn't have permission to access the object
-                    break;
-                case 'storage/canceled':
-                    // User canceled the upload
-                    break;
+    getDownloadURL(starsRef).then((url) => {
+        // Insert url into an <img> tag to "download"
+        console.log('second')
+        fotoCard.setAttribute('src', url);
+    }).catch((error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+            case 'storage/object-not-found':
+                console.log(error)
+                fotoCard.setAttribute('src', './assets/images/fotocard.png');
+                break;
+            case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break;
+            case 'storage/canceled':
+                console.log('Você cancelou o Upload antes do término')
+                // O usuário cancelou o upload
+                break;
 
-                // ...
+            // ...
 
-                case 'storage/unknown':
-                    // Unknown error occurred, inspect the server response
-                    break;
-            }
-        });
+            case 'storage/unknown':
+                // Ocorreu um erro desconhecido, inspecione a resposta do servidor
+                break;
+        }
+    });
 }
